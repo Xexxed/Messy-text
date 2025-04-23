60b572fbda33a3a1404ac2757767dcd95952d863
Xexxxed <bukhariahmed76@gmail.com>
2025-04-23 14:21:56 -0230

    fix: remove default localhost URL for HOST in constants.js

--- vite-project/src/utils/constants.js
+++ vite-project/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST = import.meta.env.VITE_HOST || "http://localhost:9001";
+export const HOST = import.meta.env.VITE_HOST;
 export const AUTH_ROUTES = "api/auth";
 export const CONTACTS_ROUTES = "api/contacts";
 export const SIGNUP = `${AUTH_ROUTES}/signup`;
